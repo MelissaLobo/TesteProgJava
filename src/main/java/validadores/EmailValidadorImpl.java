096fd966d7659f600762839414c59b9617eeeda7
Melissa Lobo <mellobomel@gmail.com>
2015-07-29 15:26:22 -0300

    Primeiro Comit

--- src/main/java/validadores/EmailValidadorImpl.java
+++ src/main/java/validadores/EmailValidadorImpl.java
@@ -11,12 +11,17 @@ public class EmailValidadorImpl implements EmailValidador {
 		String[] partesArrayString = email.split("@");
 		String parteLocal = partesArrayString[0];
 		String parteDominio = partesArrayString[1];
+		partesArrayString[1].split(".");
 
 		if (parteLocal.length() > 64 || parteLocal.length() < 4 && parteLocal.matches("//s")) {
 			return false;
 		}
 		if( parteLocal.matches("//w")) {
 			return true;
+		}
+		
+		if(parteDominio.length() > 255 || parteDominio.length() < 4 && parteDominio.matches("//s")) {
+			return false;
 		}
 			return true;
 	}
